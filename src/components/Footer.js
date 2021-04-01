a93092cb2d94da9627e0387579ca1247cfc23188
paulstgermain <paulg.stgermain@gmail.com>
2021-04-01 00:25:21 -0400

    fix footer navlinks & deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,11 +28,11 @@ export default function Footer(){
 
                 <div className='footerNav'>
                     <ul>
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>About</a></li>
-                        <li><a href='#'>Skills</a></li>
-                        <li><a href='#'>Projects</a></li>
-                        <li><a href='#'>Contact</a></li>
+                        <li><a href='#home'>Home</a></li>
+                        <li><a href='#about'>About</a></li>
+                        <li><a href='#skills'>Skills</a></li>
+                        <li><a href='#projects'>Projects</a></li>
+                        <li><a href='#contact'>Contact</a></li>
                     </ul>
                 </div>
             </div>
